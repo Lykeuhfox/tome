298794d52521c24102af20a7521d650e59edf9ef
Lykeuhfox <gbull1986@gmail.com>
2026-03-20 13:42:32 -0400

    restarting

--- src/app/features/search/search.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const SEARCH_ROUTES: Routes = [
-  {
-    path: '',
-    loadComponent: () =>
-      import('./components/search/search.component').then((m) => m.SearchComponent),
-  },
-];
